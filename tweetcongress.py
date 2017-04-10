b0d2bf785710a37293072f99752e99c5618dacb2
Sam Brady <sambrady3@gmail.com>
2017-04-10 17:02:47 -0400

    Fixed small bugs in tweetcongress.py

--- tweetcongress.py
+++ tweetcongress.py
@@ -9,17 +9,17 @@ class Tweet:
 		self.body = body
 
 # class TwitterAPICommunicator:
-	"""Handles all communications with the Twitter API.
-	This includes both posting Tweets, and listening for and receiving Tweets."""
+	#"""Handles all communications with the Twitter API.
+	#This includes both posting Tweets, and listening for and receiving Tweets."""
 
 	# def parse_message(raw_tweet):
-		"""Parses a raw tweet (JSON format) and returns the zip code requested by the user."""
+		#"""Parses a raw tweet (JSON format) and returns the zip code requested by the user."""
 
 class CongressAPICommunicator:
 	"""Handles all communications with Congressional API.
 	This includes fetching and parsing representatives by zip code, daily schedules, and live vote updates."""
 
-	def fetch_by_zipcode(zipcode):
+	def fetch_by_zipcode(self, zipcode):
 		reps = []
 
 		# TODO: This should return a list of Representative objects
@@ -34,15 +34,16 @@ class CongressAPICommunicator:
 
 		# Getting every Rep fist & last names
 		for i in range(NumOfRep):
-			representative = Representive()
-			representative.first = info['results'][i]['first_name']
-			representative.last = info['results'][i]['last_name']
-			representative.chamber = info['results'][i]['chamber']
+			first = info['results'][i]['first_name']
+			last = info['results'][i]['last_name']
+			chamber = info['results'][i]['chamber']
+			representative = Representive(first, last, chamber)
+
 			reps.append(representative)
 		return reps
 
 class Representive:
-	def__init__(self, first, last, chamber):
+	def __init__(self, first, last, chamber):
 		self.first = first
 		self.last = last
-		self.chamber
+		self.chamber = chamber
